f0a8d62636d914db6eb181c758d9f5ecac64ac41
YueweiGuo <2780381429@qq.com>
2022-03-15 13:41:50 +0800

    部分测试代码

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,11 +15,57 @@
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
-
+import axios from 'axios'
 export default {
   name: 'Home',
   components: {
     // HelloWorld
+  },
+  created() {
+    axios.post('/tour-product/imge/getUrl').then(res => {
+      console.log(res)
+    })
+    axios({
+      url: '/tour-product/sysProduct/queryProductList',
+      method: 'OPTIONS',
+      data: {
+        pageNum: 1,
+        pageSize: 10
+      },
+      headers: {
+        token: 'TG5SUFlmbTJZQWQzc2FqdVpGSXpIU1hXRzhJTVZ5OFVHb2VNSkRHQnlHWHpWL0ZjN3ZINDVyeXJRTTR4RWdkMitHVDhiSkJsU25HTHFEUmNHUjJ5bnAxS0t1NWxuekozTGJML3Z6dEQ2ZDA9'
+      }
+    }).then(res => {
+      console.log(res)
+    })
+    axios({
+      url: '/tour-product/comp/scenic/scenicJoinList',
+      method: 'OPTIONS',
+      data: {
+        pageNum: 1,
+        pageSize: 10,
+        scenicId: 942847173206605824
+      },
+      headers: {
+        token: 'TG5SUFlmbTJZQWQzc2FqdVpGSXpIU1hXRzhJTVZ5OFVHb2VNSkRHQnlHWHpWL0ZjN3ZINDVyeXJRTTR4RWdkMitHVDhiSkJsU25HTHFEUmNHUjJ5bnAxS0t1NWxuekozTGJML3Z6dEQ2ZDA9'
+      }
+    }).then(res => {
+      console.log(res)
+    })
+    // axios({
+    //   url: '/tour-product/comp/scenic/scenicJoinList',
+    //   method: 'POST',
+    //   params: {
+    //     pageNum: 1,
+    //     pageSize: 10,
+    //     scenicId: '799648908592939008'
+    //   },
+    //   headers: {
+    //     token: 'TG5SUFlmbTJZQWQzc2FqdVpGSXpIU1hXRzhJTVZ5OFVHb2VNSkRHQnlHWHpWL0ZjN3ZINDVyeXJRTTR4RWdkMitHVDhiSkJsU25HTHFEUmNHUjJ5bnAxS0t1NWxuekozTGJML3Z6dEQ2ZDA9'
+    //   }
+    // }).then(res => {
+    //   console.log(res)
+    // })
   }
 }
 </script>
---
+++ vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	devServer: {
+		proxy: 'https://zqwl-tour.cytsszt.com/',
+		// proxy: 'https://www.baidu.com/',
+		port: 8090
+	},
+	publicPath: './'
+}
